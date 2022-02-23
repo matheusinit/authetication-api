81bfc02a4e25d7f3e130daed83a6f8dfee1627d0
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-02-23 20:44:12 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -33,15 +33,15 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         username,
         email,
         password
       })
 
       return {
-        statusCode: 0,
-        body: ''
+        statusCode: 200,
+        body: account
       }
     } catch (error) {
       return serverError()
