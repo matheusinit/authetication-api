cc3802e3d7e4c569b94c672c797c10c0c855082d
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-17 18:02:59 -0300

    feat: ensure checkUsername return true if username is available

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,18 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { CheckUsernameRepository } from '../../../../data/protocols/check-username-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, CheckUsernameRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const accountResult = await accountCollection.findOne(result.insertedId)
     return MongoHelper.map(accountResult)
+  }
+
+  async checkUsername (username: string): Promise<boolean> {
+    return true
   }
 }
