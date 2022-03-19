dca20bbd71b88c7b678d88cfa746e8d5e4927c17
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-19 07:15:21 -0300

    feat: ensure checkEmail return true if email is available

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,11 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
 import { CheckUsernameRepository } from '../../../../data/protocols/check-username-repository'
+import { CheckEmailRepository } from '../../../../data/protocols/check-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository, CheckUsernameRepository {
+export class AccountMongoRepository implements AddAccountRepository, CheckUsernameRepository, CheckEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -16,5 +17,9 @@ export class AccountMongoRepository implements AddAccountRepository, CheckUserna
     const accountCollection = MongoHelper.getCollection('accounts')
     const isAvailable = await accountCollection.findOne({ username })
     return isAvailable === null
+  }
+
+  async checkEmail (email: string): Promise<boolean> {
+    return true
   }
 }
