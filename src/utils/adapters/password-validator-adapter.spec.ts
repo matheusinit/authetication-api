e136ca1f93c6f4a6fa254110e9e4faa5ce9b17ae
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-22 09:49:32 -0300

    refactor: move PasswordValidatorStub to a factory function

--- src/utils/adapters/password-validator-adapter.spec.ts
+++ src/utils/adapters/password-validator-adapter.spec.ts
@@ -1,10 +1,14 @@
 import { PasswordValidator } from '../../presentation/protocols/password-validator'
 import { PasswordValidatorAdapter } from './password-validator-adapter'
 
-class PasswordValidatorStub implements PasswordValidator {
-  isValid (password: string): boolean {
-    return true
+const makePasswordValidatorStub = (): PasswordValidator => {
+  class PasswordValidatorStub implements PasswordValidator {
+    isValid (password: string): boolean {
+      return true
+    }
   }
+
+  return new PasswordValidatorStub()
 }
 
 const makeSut = (): PasswordValidatorAdapter => {
@@ -14,7 +18,7 @@ const makeSut = (): PasswordValidatorAdapter => {
 describe('PasswordValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = makeSut()
-    const passwordValidatorStub = new PasswordValidatorStub()
+    const passwordValidatorStub = makePasswordValidatorStub()
     jest.spyOn(passwordValidatorStub, 'isValid').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_password')
     expect(isValid).toBe(false)
