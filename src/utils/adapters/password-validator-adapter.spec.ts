02cbfbbc0bb5e31751e93b9b93e0c0e181ab6310
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-22 10:25:29 -0300

    test: ensure PasswordValidator is called with correct password

--- src/utils/adapters/password-validator-adapter.spec.ts
+++ src/utils/adapters/password-validator-adapter.spec.ts
@@ -1,4 +1,5 @@
 import { PasswordValidator } from '../../presentation/protocols/password-validator'
+import { passwordValidator } from '../password-validator/password-validator'
 import { PasswordValidatorAdapter } from './password-validator-adapter'
 
 const makePasswordValidatorStub = (): PasswordValidator => {
@@ -22,5 +23,12 @@ describe('PasswordValidator Adapter', () => {
     jest.spyOn(passwordValidatorStub, 'isValid').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_password')
     expect(isValid).toBe(false)
+  })
+
+  it('Should call PasswordValidator with correct password', () => {
+    const sut = makeSut()
+    const isValidSpy = jest.spyOn(passwordValidator, 'isValid')
+    sut.isValid('any_password')
+    expect(isValidSpy).toHaveBeenCalledWith('any_password')
   })
 })
