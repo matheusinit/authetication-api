72e83981cad5accaa404531c6f6c1309c33348ab
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-22 11:12:18 -0300

    feat: add SignUpController's dependency passwordValidator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,14 +2,16 @@ import { DbAddAccount } from '../../data/usecases/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { PasswordValidatorAdapter } from '../../utils/adapters/password-validator-adapter'
+import { EmailValidatorAdapter } from '../../utils/adapters/email-validator-adapter'
 
 export const makeSignUpController = (): SignUpController => {
   const salt = 12
   const encrypter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const emailValidator = new EmailValidatorAdapter()
+  const passwordValidator = new PasswordValidatorAdapter()
   const dbAddAccount = new DbAddAccount(encrypter, accountMongoRepository, accountMongoRepository, accountMongoRepository)
-  const signUpController = new SignUpController(emailValidator, dbAddAccount)
+  const signUpController = new SignUpController(emailValidator, dbAddAccount, passwordValidator)
   return signUpController
 }
