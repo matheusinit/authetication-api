98950ed1eaeb05c12793706cb7d6c0f3eccdd82e
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-22 17:07:05 -0300

    test: ensure LoginController return 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,16 @@
+import { MissingParamError } from '../../errors'
+import { LoginController } from './login'
+
+describe('Login Controller', () => {
+  it('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+})
