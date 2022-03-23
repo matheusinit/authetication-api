6e5582c412e95a2cfec1506830410b28fa41ca02
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-23 17:50:36 -0300

    refactor: move sut and tokenGenerator creation to a factory function

--- src/data/usecases/db-auth-account/db-auth-account.spec.ts
+++ src/data/usecases/db-auth-account/db-auth-account.spec.ts
@@ -1,16 +1,30 @@
 import { TokenGenerator } from '../../protocols/token-generator'
 import { DbAuthAccount } from './db-auth-account'
 
+interface SutTypes {
+  sut: DbAuthAccount
+  tokenGeneratorStub: TokenGenerator
+}
+
+const makeSut = (): SutTypes => {
+  class TokenGeneratorStub implements TokenGenerator {
+    generate (payload: any): string {
+      return ''
+    }
+  }
+
+  const tokenGeneratorStub = new TokenGeneratorStub()
+
+  const sut = new DbAuthAccount(tokenGeneratorStub)
+  return {
+    sut,
+    tokenGeneratorStub
+  }
+}
+
 describe('DbAuthAccount', () => {
   it('Should call TokenGenerator with correct values', async () => {
-    class TokenGeneratorStub implements TokenGenerator {
-      generate (payload: any): string {
-        return ''
-      }
-    }
-
-    const tokenGeneratorStub = new TokenGeneratorStub()
-    const sut = new DbAuthAccount(tokenGeneratorStub)
+    const { sut, tokenGeneratorStub } = makeSut()
     const generateSpy = jest.spyOn(tokenGeneratorStub, 'generate')
     const accountInfo = {
       email: 'any_email@mail.com',
