d0c7366a8bba44622c55bad685c3ba972a358266
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-25 15:10:35 -0300

    refactor: organize tests by methods

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -19,53 +19,59 @@ describe('Account Mongo Repository', () => {
     return new AccountMongoRepository()
   }
 
-  it('Should return an account on success', async () => {
-    const sut = makeSut()
-    const account = await sut.add({
-      username: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  describe('add()', () => {
+    it('Should return an account on success', async () => {
+      const sut = makeSut()
+      const account = await sut.add({
+        username: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.username).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.username).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
   })
 
-  it('Should checkUsername return true if username is available', async () => {
-    const sut = makeSut()
-    const isAvailable = await sut.checkUsername('available_username')
-    expect(isAvailable).toBe(true)
+  describe('checkUsername()', () => {
+    it('Should checkUsername return true if username is available', async () => {
+      const sut = makeSut()
+      const isAvailable = await sut.checkUsername('available_username')
+      expect(isAvailable).toBe(true)
+    })
+
+    it('Should checkUsername return false if username is unavailable', async () => {
+      const sut = makeSut()
+      const fakeAccount = {
+        username: 'unavailable_username',
+        email: 'valid_email@email.com',
+        password: 'hashed_password'
+      }
+      await MongoHelper.getCollection('accounts').insertOne(fakeAccount)
+      const isAvailable = await sut.checkUsername('unavailable_username')
+      expect(isAvailable).toBe(false)
+    })
   })
 
-  it('Should checkUsername return false if username is unavailable', async () => {
-    const sut = makeSut()
-    const fakeAccount = {
-      username: 'unavailable_username',
-      email: 'valid_email@email.com',
-      password: 'hashed_password'
-    }
-    await MongoHelper.getCollection('accounts').insertOne(fakeAccount)
-    const isAvailable = await sut.checkUsername('unavailable_username')
-    expect(isAvailable).toBe(false)
-  })
+  describe('checkEmail()', () => {
+    it('Should checkEmail return true if email is available', async () => {
+      const sut = makeSut()
+      const isAvailable = await sut.checkEmail('available_email@mail.com')
+      expect(isAvailable).toBe(true)
+    })
 
-  it('Should checkEmail return true if email is available', async () => {
-    const sut = makeSut()
-    const isAvailable = await sut.checkEmail('available_email@mail.com')
-    expect(isAvailable).toBe(true)
-  })
-
-  it('Should checkEmail return false if email is unavailable', async () => {
-    const sut = makeSut()
-    const fakeAccount = {
-      username: 'valid_username',
-      email: 'unavailable_email@email.com',
-      password: 'hashed_password'
-    }
-    await MongoHelper.getCollection('accounts').insertOne(fakeAccount)
-    const isAvailable = await sut.checkEmail('unavailable_email@email.com')
-    expect(isAvailable).toBe(false)
+    it('Should checkEmail return false if email is unavailable', async () => {
+      const sut = makeSut()
+      const fakeAccount = {
+        username: 'valid_username',
+        email: 'unavailable_email@email.com',
+        password: 'hashed_password'
+      }
+      await MongoHelper.getCollection('accounts').insertOne(fakeAccount)
+      const isAvailable = await sut.checkEmail('unavailable_email@email.com')
+      expect(isAvailable).toBe(false)
+    })
   })
 })
