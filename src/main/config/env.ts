f8670e8d5e8345445d30254c559df1e6c3f11027
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-26 11:45:42 -0300

    chore: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,7 @@
+import 'dotenv/config'
+
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/authentication-api',
-  port: process.env.PORT ?? 3000
+  port: process.env.PORT ?? 3000,
+  secret: process.env.secret
 }
