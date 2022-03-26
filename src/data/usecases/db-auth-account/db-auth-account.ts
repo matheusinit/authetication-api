27ded166b9786c430e5c9428fe5067a33e2994ab
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-26 09:07:02 -0300

    feat: ensure DbAuthAccount call HashComparator with correct values

--- src/data/usecases/db-auth-account/db-auth-account.ts
+++ src/data/usecases/db-auth-account/db-auth-account.ts
@@ -1,18 +1,27 @@
 import { AuthAccount, Credentials } from '../../../domain/usecases/auth-account'
+import { HashComparator } from '../../protocols/hash-comparator'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { TokenGenerator } from '../../protocols/token-generator'
 
 export class DbAuthAccount implements AuthAccount {
   private readonly tokenGenerator: TokenGenerator
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly hashComparator: HashComparator
 
-  constructor (tokenGenerator: TokenGenerator, loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor (
+    tokenGenerator: TokenGenerator,
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashComparator: HashComparator
+  ) {
     this.tokenGenerator = tokenGenerator
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashComparator = hashComparator
   }
 
   async auth (credentials: Credentials): Promise<String> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(credentials.email)
+
+    this.hashComparator.compare(credentials.password, account.password)
 
     const token = this.tokenGenerator.generate({
       id: account.id,
