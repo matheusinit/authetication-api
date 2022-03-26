b022c2432893991aaabac0527fec3aeccac48ff8
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-26 11:55:54 -0300

    feat: ensure generate() return a token on success

--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -4,7 +4,6 @@ import { TokenGenerator } from '../../data/protocols/token-generator'
 
 export class JwtAdapter implements TokenGenerator {
   generate (payload: any): string {
-    jwt.sign(payload, env.secret)
-    return null
+    return jwt.sign(payload, env.secret)
   }
 }
