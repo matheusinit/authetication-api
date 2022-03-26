7433b4d36d5f5a4df0d30a006b621d2c7ea3580b
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-26 12:23:20 -0300

    test: ensure Login route return a token on success

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,38 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from './../config/app'
+
+import bcrypt from 'bcrypt'
+
+let collection: Collection
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    collection = MongoHelper.getCollection('accounts')
+    await collection.deleteMany({})
+  })
+
+  it('Should return a token on success', async () => {
+    const fakeAccount = {
+      username: 'Matheus Oliveira',
+      email: 'matheus.oliveira@gmail.com',
+      password: await bcrypt.hash('senha123', 12),
+      status: 'inactive'
+    }
+    await collection.insertOne(fakeAccount)
+
+    await request(app).post('/api/login').send({
+      email: 'matheus.oliveira@gmail.com',
+      password: 'senha123'
+    }).expect(200)
+  })
+})
