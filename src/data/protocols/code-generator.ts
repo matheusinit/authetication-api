534aba77a83d71bd73420b779bb48f201cd5505e
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-29 14:21:15 -0300

    feat: ensure DbSendConfirmationCode calls CodeGenerator

---
+++ src/data/protocols/code-generator.ts
@@ -0,0 +1,3 @@
+export interface CodeGenerator {
+  generate: () => string
+}
--- src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.ts
+++ src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.ts
@@ -2,18 +2,22 @@ import { SendConfirmationCode } from '../../../domain/usecases/send-confirmation
 import { AccountIsActiveError } from '../../errors/account-is-active-error'
 import { EmailNotRegisteredError } from '../../errors/email-not-registered-error'
 import { CheckEmailRepository } from '../../protocols/check-email-repository'
+import { CodeGenerator } from '../../protocols/code-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 
 export class DbSendConfirmationCode implements SendConfirmationCode {
   private readonly checkEmailRepository: CheckEmailRepository
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly codeGenerator: CodeGenerator
 
   constructor (
     checkEmailRepository: CheckEmailRepository,
-    loadAccountByEmailRepository: LoadAccountByEmailRepository
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    codeGenerator: CodeGenerator
   ) {
     this.checkEmailRepository = checkEmailRepository
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.codeGenerator = codeGenerator
   }
 
   async send (email: string): Promise<void> {
@@ -28,5 +32,7 @@ export class DbSendConfirmationCode implements SendConfirmationCode {
     if (account.status === 'active') {
       throw new AccountIsActiveError()
     }
+
+    this.codeGenerator.generate()
   }
 }
