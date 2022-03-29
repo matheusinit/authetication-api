15387d88b8220554ea87c3ec5ed0b99e39334821
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-29 11:49:07 -0300

    refactor: move sut creation to a factory function

--- src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.spec.ts
+++ src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.spec.ts
@@ -2,15 +2,28 @@ import { EmailNotRegisteredError } from '../../errors/email-not-registered-error
 import { CheckEmailRepository } from '../../protocols/check-email-repository'
 import { DbSendConfirmationCode } from './db-send-confirmation-code'
 
+interface SutTypes {
+  sut: DbSendConfirmationCode
+  checkEmailRepositoryStub: CheckEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  class CheckEmailRepositoryStub implements CheckEmailRepository {
+    async checkEmail (email: string): Promise<boolean> {
+      return true
+    }
+  }
+  const checkEmailRepositoryStub = new CheckEmailRepositoryStub()
+  const sut = new DbSendConfirmationCode(checkEmailRepositoryStub)
+  return {
+    sut,
+    checkEmailRepositoryStub
+  }
+}
+
 describe('DbSendConfirmationCode', () => {
   it('Should throw an error if email is not registered', async () => {
-    class CheckEmailRepositoryStub implements CheckEmailRepository {
-      async checkEmail (email: string): Promise<boolean> {
-        return true
-      }
-    }
-    const checkEmailRepositoryStub = new CheckEmailRepositoryStub()
-    const sut = new DbSendConfirmationCode(checkEmailRepositoryStub)
+    const { sut } = makeSut()
     const promise = sut.send('any_email@mail.com')
     await expect(promise).rejects.toThrow(new EmailNotRegisteredError())
   })
