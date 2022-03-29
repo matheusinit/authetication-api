ec55fbaeedbba835bb957cd4d986c59c2c4122d6
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-29 09:37:23 -0300

    feat: ensure SendConfirmationController returns 500 if SendConfirmationCode throws

--- src/presentation/controllers/send-confirmation-code/send-confirmation-code.ts
+++ src/presentation/controllers/send-confirmation-code/send-confirmation-code.ts
@@ -1,6 +1,6 @@
 import { SendConfirmationCode } from '../../../domain/usecases/send-confirmation-code'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, ok } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -14,20 +14,24 @@ export class SendConfirmationCodeController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email } = httpRequest.body
+    try {
+      const { email } = httpRequest.body
 
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
+      if (!email) {
+        return badRequest(new MissingParamError('email'))
+      }
+
+      const isEmailValid = this.emailValidator.isValid(email)
+
+      if (!isEmailValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      await this.sendConfirmationCode.send(email)
+
+      return ok({ message: 'Email with code sent' })
+    } catch (error) {
+      return serverError()
     }
-
-    const isEmailValid = this.emailValidator.isValid(email)
-
-    if (!isEmailValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    await this.sendConfirmationCode.send(email)
-
-    return ok({ message: 'Email with code sent' })
   }
 }
