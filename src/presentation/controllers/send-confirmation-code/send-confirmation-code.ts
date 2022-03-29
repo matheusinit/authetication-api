58dfdf0f5c383eb9cbd071752522178bd0a51cf5
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-29 07:33:20 -0300

    feat: ensure SendConfirmationCodeController returns 400 if email provided is invalid

--- src/presentation/controllers/send-confirmation-code/send-confirmation-code.ts
+++ src/presentation/controllers/send-confirmation-code/send-confirmation-code.ts
@@ -1,9 +1,24 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class SendConfirmationCodeController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
+    }
+
+    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isEmailValid) {
+      return badRequest(new InvalidParamError('email'))
+    }
   }
 }
