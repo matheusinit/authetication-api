35c1f28224d7609b55b5c624380106ad1750da7f
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-29 12:15:01 -0300

    feat: ensure DbSendConfirmationCode call LoadAccountByEmailRepository with correct email

--- src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.ts
+++ src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.ts
@@ -1,12 +1,18 @@
 import { SendConfirmationCode } from '../../../domain/usecases/send-confirmation-code'
 import { EmailNotRegisteredError } from '../../errors/email-not-registered-error'
 import { CheckEmailRepository } from '../../protocols/check-email-repository'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 
 export class DbSendConfirmationCode implements SendConfirmationCode {
   private readonly checkEmailRepository: CheckEmailRepository
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
 
-  constructor (checkEmailRepository: CheckEmailRepository) {
+  constructor (
+    checkEmailRepository: CheckEmailRepository,
+    loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) {
     this.checkEmailRepository = checkEmailRepository
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
   }
 
   async send (email: string): Promise<void> {
@@ -15,5 +21,7 @@ export class DbSendConfirmationCode implements SendConfirmationCode {
     if (isExistentEmail) {
       throw new EmailNotRegisteredError()
     }
+
+    await this.loadAccountByEmailRepository.loadByEmail(email)
   }
 }
