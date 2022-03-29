08c2c3afb09fa81d627e40cdb3698af34390da88
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-29 07:34:51 -0300

    test: ensure SendConfirmationCodeController returns 400 if email provided is invalid

--- src/presentation/controllers/send-confirmation-code/send-confirmation-code.spec.ts
+++ src/presentation/controllers/send-confirmation-code/send-confirmation-code.spec.ts
@@ -1,18 +1,49 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { EmailValidator } from '../signup/signup-protocols'
 import { SendConfirmationCodeController } from './send-confirmation-code'
 
-const makeSut = (): SendConfirmationCodeController => {
-  return new SendConfirmationCodeController()
+interface SutTypes {
+  sut: SendConfirmationCodeController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SendConfirmationCodeController(emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('SendConfirmartionCode Controller', () => {
   it('Should return 400 if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: { }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  it('Should return 400 if email provided is invalid', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@mail.com'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
