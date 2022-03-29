f2991ec1016aa8952cc98a96a832f79acd2d15ea
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-29 11:59:33 -0300

    test: ensure DbSendConfirmationCode should call CheckEmailRepository with correct email

--- src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.spec.ts
+++ src/data/usecases/db-send-confirmation-code/db-send-confirmation-code.spec.ts
@@ -10,7 +10,7 @@ interface SutTypes {
 const makeCheckEmailRepositoryStub = (): CheckEmailRepository => {
   class CheckEmailRepositoryStub implements CheckEmailRepository {
     async checkEmail (email: string): Promise<boolean> {
-      return true
+      return false
     }
   }
   return new CheckEmailRepositoryStub()
@@ -26,8 +26,16 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbSendConfirmationCode', () => {
+  it('Should call CheckEmailRepository with correct email', async () => {
+    const { sut, checkEmailRepositoryStub } = makeSut()
+    const checkEmailSpy = jest.spyOn(checkEmailRepositoryStub, 'checkEmail')
+    await sut.send('any_email@mail.com')
+    expect(checkEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
   it('Should throw an error if email is not registered', async () => {
-    const { sut } = makeSut()
+    const { sut, checkEmailRepositoryStub } = makeSut()
+    jest.spyOn(checkEmailRepositoryStub, 'checkEmail').mockReturnValueOnce(new Promise(resolve => resolve(true)))
     const promise = sut.send('any_email@mail.com')
     await expect(promise).rejects.toThrow(new EmailNotRegisteredError())
   })
