3aa65436c9f8b35ac8724a48ec542175611a6ab1
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-30 17:18:15 -0300

    refactor: move sut creation to a factory function

--- src/presentation/middlewares/authentication-middleware.spec.ts
+++ src/presentation/middlewares/authentication-middleware.spec.ts
@@ -1,15 +1,29 @@
 import { TokenValidator } from '../protocols/token-validator'
 import { AuthenticationMiddleware } from './authentication-middleware'
 
+interface SutTypes {
+  sut: AuthenticationMiddleware
+  tokenValidatorStub: TokenValidator
+}
+
+const makeSut = (): SutTypes => {
+  class TokenValidatorStub implements TokenValidator {
+    async verify (token: string): Promise<boolean> {
+      return true
+    }
+  }
+  const tokenValidatorStub = new TokenValidatorStub()
+  const sut = new AuthenticationMiddleware(tokenValidatorStub)
+
+  return {
+    sut,
+    tokenValidatorStub
+  }
+}
+
 describe('Authentication Middleware', () => {
   it('Should call TokenValidator with correct token', async () => {
-    class TokenValidatorStub implements TokenValidator {
-      async verify (token: string): Promise<boolean> {
-        return true
-      }
-    }
-    const tokenValidatorStub = new TokenValidatorStub()
-    const sut = new AuthenticationMiddleware(tokenValidatorStub)
+    const { sut, tokenValidatorStub } = makeSut()
     const verifySpy = jest.spyOn(tokenValidatorStub, 'verify')
     const httpRequest = {
       token: 'any_token'
