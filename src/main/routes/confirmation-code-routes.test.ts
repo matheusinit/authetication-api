e4798163a910f1402bf5392c39a5185703049873
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-30 11:28:37 -0300

    test: ensure Confirmation Code route returns a message on success

---
+++ src/main/routes/confirmation-code-routes.test.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import bcrypt from 'bcrypt'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let collection: Collection
+
+describe('ConfirmationCode Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    jest.setTimeout(60000)
+    collection = MongoHelper.getCollection('accounts')
+    await collection.deleteMany({})
+  })
+
+  it('Should return a message on success', async () => {
+    const fakeAccount = {
+      username: 'Matheus Oliveira',
+      email: 'matheus.oliveira@gmail.com',
+      password: await bcrypt.hash('senha123', 12),
+      status: 'inactive'
+    }
+    await collection.insertOne(fakeAccount)
+
+    await request(app).post('/api/account/confirmation').send({
+      email: 'matheus.oliveira@gmail.com'
+    }).expect(200)
+  })
+})
