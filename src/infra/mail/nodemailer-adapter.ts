855599b7489a647773cf8ecab31eb58b9cf94c38
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-30 09:27:37 -0300

    refactor: use environment variables for sending emails

--- src/infra/mail/nodemailer-adapter.ts
+++ src/infra/mail/nodemailer-adapter.ts
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer'
 import { EmailSender } from '../../data/protocols/email-sender'
+import env from '../../main/config/env'
 
 export const smtpOptions = {
   host: 'smtp.ethereal.email',
@@ -13,11 +14,11 @@ export const smtpOptions = {
 export class NodemailerAdapter implements EmailSender {
   async sendEmail (to: string, content: any): Promise<void> {
     const transport = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
+      host: env.smtpHost,
+      port: env.smtpPort,
       auth: {
-        user: 'easter.willms17@ethereal.email',
-        pass: 'F9ANt1zxbGcRkpkZA7'
+        user: env.smtpUser,
+        pass: env.smtpPass
       }
     })
     await transport.sendMail({
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,9 @@ import 'dotenv/config'
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/authentication-api',
   port: process.env.PORT ?? 3000,
-  secret: process.env.SECRET
+  secret: process.env.SECRET,
+  smtpHost: process.env.SMTP_HOST ?? 'smtp.ethereal.email',
+  smtpPort: process.env.SMTP_PORT as unknown as number ?? 587,
+  smtpUser: process.env.SMTP_USER ?? 'hassan.schmitt61@ethereal.email',
+  smtpPass: process.env.SMTP_PASS ?? 'x3UbzsQPyR5M6wfA4d'
 }
