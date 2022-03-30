78d56d3b0d70109c025434fd959ef3e3b9b48756
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-30 09:14:52 -0300

    refactor: change environment variable to uppercase

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,5 @@ import 'dotenv/config'
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/authentication-api',
   port: process.env.PORT ?? 3000,
-  secret: process.env.secret
+  secret: process.env.SECRET
 }
