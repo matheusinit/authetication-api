fa1c27834f9c7d7421bc1c051d8cb5f7d460e71a
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2022-03-31 20:23:22 -0300

    feat: ensure JwtAdapter call Jwt with correct values

--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -1,9 +1,15 @@
 import jwt from 'jsonwebtoken'
 import env from './../../main/config/env'
 import { TokenGenerator } from '../../data/protocols/token-generator'
+import { TokenValidator } from '../../presentation/protocols/token-validator'
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements TokenGenerator, TokenValidator {
   generate (payload: any): string {
     return jwt.sign(payload, env.secret)
+  }
+
+  async verify (token: string): Promise<boolean> {
+    jwt.verify(token, process.env.SECRET)
+    return null
   }
 }
