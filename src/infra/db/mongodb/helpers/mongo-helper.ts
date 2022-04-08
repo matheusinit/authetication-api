532f241c346884c38a3f156397c05505babfce8a
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-08 18:04:04 -0300

    refactor: put id as first item of object

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -16,6 +16,6 @@ export const MongoHelper = {
   },
   map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
-    return Object.assign({}, collectionWithoutId, { id: _id })
+    return Object.assign({ id: null }, collectionWithoutId, { id: _id })
   }
 }
