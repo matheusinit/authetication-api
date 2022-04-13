54b1688039758309cc81b71ba2ccebeb17c2bf58
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-13 17:29:56 -0300

    test: ensure loadByEmail() return a confirmation code on success

--- src/infra/db/mongodb/confirmation-code/confirmation-code-repository.spec.ts
+++ src/infra/db/mongodb/confirmation-code/confirmation-code-repository.spec.ts
@@ -26,4 +26,40 @@ describe('ConfirmationCode Mongo Repository', () => {
       expect(result).toBe(undefined)
     })
   })
+
+  describe('loadByEmail', () => {
+    it('Should return a confirmation code on success', async () => {
+      const sut = new ConfirmationCodeRepository()
+
+      // Insert fake account
+      const fakeAccount = {
+        username: 'any_username',
+        email: 'any_email@mail.com',
+        password: 'hashed_password',
+        status: 'any_status'
+      }
+      const accountCollection = MongoHelper.getCollection('accounts')
+      await accountCollection.insertOne(fakeAccount)
+
+      // Insert fake confirmation code
+      const fakeConfirmationCode = {
+        code: 'any_code',
+        created_at: new Date()
+      }
+      const { insertedId } = await collection.insertOne(fakeConfirmationCode)
+
+      // Update confirmation code of fake account
+      await accountCollection.findOneAndUpdate(
+        { email: 'any_email@mail.com' },
+        {
+          $set: { code_id: insertedId }
+        }
+      )
+
+      const confirmationCode = await sut.loadByEmail('any_email@mail.com')
+
+      expect(confirmationCode).toBeTruthy()
+      expect(confirmationCode.code).toBe('any_code')
+    })
+  })
 })
