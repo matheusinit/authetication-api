e4ce8e82103bbb08ce3009366f79ce4333cb0e11
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-14 11:16:42 -0300

    test: ensure ActivateAccount route return 200 on success

--- src/main/routes/confirmation-code-routes.test.ts
+++ src/main/routes/confirmation-code-routes.test.ts
@@ -6,7 +6,8 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 import env from '../config/env'
 
-let collection: Collection
+let accountCollection: Collection
+let confirmationCodeCollection: Collection
 
 describe('ConfirmationCode Routes', () => {
   beforeAll(async () => {
@@ -18,69 +19,102 @@ describe('ConfirmationCode Routes', () => {
   })
 
   beforeEach(async () => {
-    collection = MongoHelper.getCollection('accounts')
-    await collection.deleteMany({})
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+
+    confirmationCodeCollection = MongoHelper.getCollection('confirmation-code')
+    await confirmationCodeCollection.deleteMany({})
   })
 
-  it('Should return a message on success', async () => {
-    const fakeAccount = {
-      username: 'Matheus Oliveira',
-      email: 'matheus.oliveira@gmail.com',
-      password: await bcrypt.hash('senha123', 12),
-      status: 'inactive'
-    }
-    const { insertedId: id } = await collection.insertOne(fakeAccount)
+  describe('Send confirmation code', () => {
+    it('Should return a message on success', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'inactive'
+      }
+      const { insertedId: id } = await accountCollection.insertOne(fakeAccount)
 
-    const token = jwt.sign({ id, email: 'matheus.oliveira@gmail.com' }, env.secret)
+      const token = jwt.sign({ id, email: 'matheus.oliveira@gmail.com' }, env.secret)
 
-    await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
-      email: 'matheus.oliveira@gmail.com'
-    }).expect(200)
-  }, 60000)
+      await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
+        email: 'matheus.oliveira@gmail.com'
+      }).expect(200)
+    }, 60000)
 
-  it('Should return a bad request if email is not registered', async () => {
-    const fakeAccount = {
-      username: 'Matheus Oliveira',
-      email: 'matheus.oliveira@gmail.com',
-      password: await bcrypt.hash('senha123', 12),
-      status: 'inactive'
-    }
-    const { insertedId: id } = await collection.insertOne(fakeAccount)
+    it('Should return a bad request if email is not registered', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'inactive'
+      }
+      const { insertedId: id } = await accountCollection.insertOne(fakeAccount)
 
-    const token = jwt.sign({ id, email: 'matheus.oliveira@gmail.com' }, env.secret)
+      const token = jwt.sign({ id, email: 'matheus.oliveira@gmail.com' }, env.secret)
 
-    await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
-      email: 'matheus.oliveira1@gmail.com'
-    }).expect(400)
+      await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
+        email: 'matheus.oliveira1@gmail.com'
+      }).expect(400)
+    })
+
+    it('Should return a bad request if account is already active', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'active'
+      }
+      const { insertedId: id } = await accountCollection.insertOne(fakeAccount)
+
+      const token = jwt.sign({ id, email: 'matheus.oliveira@gmail.com' }, env.secret)
+
+      await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
+        email: 'matheus.oliveira@gmail.com'
+      }).expect(400)
+    })
+
+    it('Should return a unauthorized if authentication token is not provided', async () => {
+      await request(app).post('/api/account/confirmation').send({
+        email: 'matheus.oliveira@gmai.com'
+      }).expect(401)
+    })
+
+    it('Should return an unauthorized if authentication token is invalid', async () => {
+      const token = jwt.sign({ id: 'valid_id', email: 'any_email' }, 'any_secret')
+
+      await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
+        email: 'matheus.oliveira@gmai.com'
+      }).expect(401)
+    })
   })
 
-  it('Should return a bad request if account is already active', async () => {
-    const fakeAccount = {
-      username: 'Matheus Oliveira',
-      email: 'matheus.oliveira@gmail.com',
-      password: await bcrypt.hash('senha123', 12),
-      status: 'active'
-    }
-    const { insertedId: id } = await collection.insertOne(fakeAccount)
+  describe('Activate account by confirmation code', () => {
+    it('Should return 200 on success', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'inactive'
+      }
+      const { insertedId: id } = await accountCollection.insertOne(fakeAccount)
 
-    const token = jwt.sign({ id, email: 'matheus.oliveira@gmail.com' }, env.secret)
+      const fakeConfirmationCode = {
+        code: '765D6E85',
+        createdAt: new Date()
+      }
 
-    await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
-      email: 'matheus.oliveira@gmail.com'
-    }).expect(400)
-  })
+      const { insertedId: codeId } = await confirmationCodeCollection.insertOne(fakeConfirmationCode)
 
-  it('Should return a unauthorized if authentication token is not provided', async () => {
-    await request(app).post('/api/account/confirmation').send({
-      email: 'matheus.oliveira@gmai.com'
-    }).expect(401)
-  })
+      await accountCollection.findOneAndUpdate({ email: 'matheus.oliveira@gmail.com' }, { $set: { code_id: codeId } })
 
-  it('Should return an unauthorized if authentication token is invalid', async () => {
-    const token = jwt.sign({ id: 'valid_id', email: 'any_email' }, 'any_secret')
+      const token = jwt.sign({ id, email: 'matheus.oliveira@gmail.com' }, env.secret)
 
-    await request(app).post('/api/account/confirmation').set('Authorization', `Bearer: ${token}`).send({
-      email: 'matheus.oliveira@gmai.com'
-    }).expect(401)
+      await request(app).post('/api/account/activate').set('Authorization', `Bearer: ${token}`).send({
+        email: 'matheus.oliveira@gmail.com',
+        code: '765D6E85'
+      }).expect(200)
+    })
   })
 })
