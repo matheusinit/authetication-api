21717148ffc581a27909c7750893130c4a5934eb
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-14 07:58:18 -0300

    feat: ensure ActivateAccountController return 400 if confirmation code is not provided

--- src/presentation/controllers/activate-account/activate-account.ts
+++ src/presentation/controllers/activate-account/activate-account.ts
@@ -4,6 +4,13 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class ActivateAccountController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    const { email, code } = httpRequest.body
+
+    if (!email) {
+      return badRequest(new MissingParamError('email'))
+    }
+    if (!code) {
+      return badRequest(new MissingParamError('confirmation code'))
+    }
   }
 }
