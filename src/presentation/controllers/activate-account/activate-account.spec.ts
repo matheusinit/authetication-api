ce8de2e602729407175c3fa6bf9ee8c9df5fbb54
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-14 07:53:18 -0300

    refactor: move sut creation to a factory function

--- src/presentation/controllers/activate-account/activate-account.spec.ts
+++ src/presentation/controllers/activate-account/activate-account.spec.ts
@@ -2,9 +2,13 @@ import { MissingParamError } from '../../errors'
 import { appError } from '../../helpers/error-helper'
 import { ActivateAccountController } from './activate-account'
 
+const makeSut = (): ActivateAccountController => {
+  return new ActivateAccountController()
+}
+
 describe('ActivateAccount Controller', () => {
   it('Should return 400 if email is not provided', async () => {
-    const sut = new ActivateAccountController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
