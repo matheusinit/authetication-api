5017dff9905a2d0e8c6a4897ba523af2b9f063ea
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-14 07:40:08 -0300

    refactor: improve variable name

--- src/infra/db/mongodb/confirmation-code/confirmation-code-repository.ts
+++ src/infra/db/mongodb/confirmation-code/confirmation-code-repository.ts
@@ -7,14 +7,14 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class ConfirmationCodeRepository implements StoreConfirmationCodeRepository, LoadConfirmationCodeByEmailRepository {
   async storeConfirmationCode (confirmationCode: string, email: string): Promise<void> {
     const collection = MongoHelper.getCollection('confirmation-code')
-    const userCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = MongoHelper.getCollection('accounts')
 
     const result = await collection.insertOne({
       code: confirmationCode,
       created_at: new Date()
     })
 
-    await userCollection.findOneAndUpdate({ email }, { $set: { code_id: result.insertedId } })
+    await accountCollection.findOneAndUpdate({ email }, { $set: { code_id: result.insertedId } })
   }
 
   async loadByEmail (email: string): Promise<ConfirmationCode> {
