c5f776d1ceb7f577f2618b1767b375c739a76d0f
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-19 14:41:25 -0300

    refactor: move sut and its dependencies to a factory function

--- src/data/usecases/db-reset-password/db-reset-password.spec.ts
+++ src/data/usecases/db-reset-password/db-reset-password.spec.ts
@@ -2,22 +2,36 @@ import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-em
 import { AccountModel } from '../db-add-account/db-add-account-protocols'
 import { DbResetPassword } from './db-reset-password'
 
-class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-  async loadByEmail (email: string): Promise<AccountModel> {
-    return {
-      id: 'any_id',
-      username: 'any_username',
-      email: 'any_email',
-      password: 'hashed_password',
-      status: 'any_status'
+interface SutTypes {
+  sut: DbResetPassword
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    async loadByEmail (email: string): Promise<AccountModel> {
+      return {
+        id: 'any_id',
+        username: 'any_username',
+        email: 'any_email',
+        password: 'hashed_password',
+        status: 'any_status'
+      }
     }
+  }
+
+  const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+  const sut = new DbResetPassword(loadAccountByEmailRepositoryStub)
+
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
   }
 }
 
 describe('DbResetPassword Usecase', () => {
   it('Should call LoadAccountByEmailRepository with correct email', async () => {
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
-    const sut = new DbResetPassword(loadAccountByEmailRepositoryStub)
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadByEmailSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail')
 
     await sut.reset('any_email@email.com', 'any_password')
