53e94b9e3929f5807a286cb127f52a3fae0265e1
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-20 11:28:16 -0300

    test: ensure ResetPasswordController return 400 if no password confirmation is provided

--- src/presentation/controllers/reset-password/reset-password.spec.ts
+++ src/presentation/controllers/reset-password/reset-password.spec.ts
@@ -32,4 +32,19 @@ describe('ResetPassword Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(appError(new MissingParamError('password')))
   })
+
+  it('Should return 400 if no password confirmation is provided', async () => {
+    const sut = new ResetPasswordController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(appError(new MissingParamError('passwordConfirmation')))
+  })
 })
