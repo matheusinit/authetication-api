47ad769d8a8d67c81dc66df7e433d7dcdef93f40
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-20 08:39:45 -0300

    feat: ensure DbResetPassword should call UpdateAccountRepository with correct values

--- src/data/usecases/db-reset-password/db-reset-password.ts
+++ src/data/usecases/db-reset-password/db-reset-password.ts
@@ -1,13 +1,16 @@
 import { ResetPassword } from '../../../domain/usecases/reset-password'
 import { EmailNotRegisteredError } from '../../errors/email-not-registered-error'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { UpdateAccountRepository } from '../../protocols/update-account-repository'
 import { AccountModel } from '../db-add-account/db-add-account-protocols'
 
 export class DbResetPassword implements ResetPassword {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly updateAccountRepository: UpdateAccountRepository
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, updateAccountRepository: UpdateAccountRepository) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.updateAccountRepository = updateAccountRepository
   }
 
   async reset (email: string, password: string): Promise<AccountModel> {
@@ -20,6 +23,8 @@ export class DbResetPassword implements ResetPassword {
     if (account.status === 'inactive') {
       throw new Error('Account is inactive')
     }
+
+    await this.updateAccountRepository.update(account.id, { password: 'new_hashed_password' })
 
     return null
   }
