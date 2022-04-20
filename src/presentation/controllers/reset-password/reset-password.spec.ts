b540b85626f4efd65d9df3c54126ca92dd3692f1
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-20 11:34:34 -0300

    test: ensure ResetPasswordController return 400 if password is not equal to password confirmation

--- src/presentation/controllers/reset-password/reset-password.spec.ts
+++ src/presentation/controllers/reset-password/reset-password.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '../../errors'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { appError } from '../../helpers/error-helper'
 import { ResetPasswordController } from './reset-password'
@@ -46,5 +47,21 @@ describe('ResetPassword Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(appError(new MissingParamError('passwordConfirmation')))
+  })
+
+  it('Should return 400 if password is not equal to password confirmation', async () => {
+    const sut = new ResetPasswordController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'another_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(appError(new InvalidParamError('passwordConfirmation')))
   })
 })
