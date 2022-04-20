617c2e9a92af193dd9e6e35770047215854b1e44
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-20 08:56:19 -0300

    feat: ensure DbResetPassword should call Encrypter with correct password

--- src/data/usecases/db-reset-password/db-reset-password.ts
+++ src/data/usecases/db-reset-password/db-reset-password.ts
@@ -2,15 +2,21 @@ import { ResetPassword } from '../../../domain/usecases/reset-password'
 import { EmailNotRegisteredError } from '../../errors/email-not-registered-error'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { UpdateAccountRepository } from '../../protocols/update-account-repository'
-import { AccountModel } from '../db-add-account/db-add-account-protocols'
+import { AccountModel, Encrypter } from '../db-add-account/db-add-account-protocols'
 
 export class DbResetPassword implements ResetPassword {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly updateAccountRepository: UpdateAccountRepository
+  private readonly encrypter: Encrypter
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, updateAccountRepository: UpdateAccountRepository) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    updateAccountRepository: UpdateAccountRepository,
+    encrypter: Encrypter
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.updateAccountRepository = updateAccountRepository
+    this.encrypter = encrypter
   }
 
   async reset (email: string, password: string): Promise<AccountModel> {
@@ -23,6 +29,8 @@ export class DbResetPassword implements ResetPassword {
     if (account.status === 'inactive') {
       throw new Error('Account is inactive')
     }
+
+    await this.encrypter.encrypt(password)
 
     await this.updateAccountRepository.update(account.id, { password: 'new_hashed_password' })
 
