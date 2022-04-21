5cfbe33b0d2901ef02af49be3de734b28f326111
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-21 08:01:12 -0300

    test: ensure ResetPasswordController call EmailValidator with correct email

--- src/presentation/controllers/reset-password/reset-password.spec.ts
+++ src/presentation/controllers/reset-password/reset-password.spec.ts
@@ -2,14 +2,32 @@ import { InvalidParamError } from '../../errors'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { appError } from '../../helpers/error-helper'
 import { ResetPasswordController } from './reset-password'
+import { EmailValidator } from '../../protocols/email-validator'
 
-const makeSut = (): ResetPasswordController => {
-  return new ResetPasswordController()
+interface SutTypes {
+  sut: ResetPasswordController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new ResetPasswordController(emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('ResetPassword Controller', () => {
   it('Should return 400 if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password',
@@ -24,7 +42,7 @@ describe('ResetPassword Controller', () => {
   })
 
   it('Should return 400 if no password is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -39,7 +57,7 @@ describe('ResetPassword Controller', () => {
   })
 
   it('Should return 400 if no password confirmation is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -54,7 +72,7 @@ describe('ResetPassword Controller', () => {
   })
 
   it('Should return 400 if password is not equal to password confirmation', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -67,5 +85,21 @@ describe('ResetPassword Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(appError(new InvalidParamError('passwordConfirmation')))
+  })
+
+  it('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 })
