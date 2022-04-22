04f0418018527513e7eabd505e9b5c05cb35a092
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-22 11:58:56 -0300

    test: ensure ResetPassword routes return an account on success

---
+++ src/main/routes/reset-password.test.ts
@@ -0,0 +1,38 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from './../config/app'
+import bcrypt from 'bcrypt'
+
+let accountCollection: Collection
+
+describe('ResetPassword Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  it('Should return an account on success', async () => {
+    const fakeAccount = {
+      username: 'Matheus Oliveira',
+      email: 'matheus.oliveira@gmail.com',
+      password: await bcrypt.hash('senha123', 12),
+      status: 'active'
+    }
+    await accountCollection.insertOne(fakeAccount)
+
+    await request(app).put('/api/account/reset-password').send({
+      email: 'matheus.oliveira@gmail.com',
+      password: 'Senhaa.123',
+      passwordConfirmation: 'Senhaa.123'
+    }).expect(200)
+  })
+})
