322019b9f15315cbd4dc9ccc04391fbb7aded0f8
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-22 15:30:06 -0300

    feat: ensure DbSendResetPasswordEmail call HashGenerator

--- src/data/usecases/db-send-reset-password-email/db-send-reset-password-email.ts
+++ src/data/usecases/db-send-reset-password-email/db-send-reset-password-email.ts
@@ -2,12 +2,15 @@ import { SendResetPasswordEmail } from '../../../domain/usecases/send-reset-pass
 import { EmailNotRegisteredError } from '../../errors/email-not-registered-error'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { AccountError } from '../../errors/account-error'
+import { HashGenerator } from '../../protocols/hash-generator'
 
 export class DbSendResetPasswordEmail implements SendResetPasswordEmail {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private readonly hashGenerator: HashGenerator
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashGenerator: HashGenerator) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashGenerator = hashGenerator
   }
 
   async send (email: string): Promise<void> {
@@ -20,5 +23,7 @@ export class DbSendResetPasswordEmail implements SendResetPasswordEmail {
     if (account.status === 'inactive') {
       throw new AccountError('Account is inactive', 'AccountIsInactiveError')
     }
+
+    this.hashGenerator.generate()
   }
 }
