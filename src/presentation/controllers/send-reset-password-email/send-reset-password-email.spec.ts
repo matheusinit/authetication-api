a79008fde2af9fd160a4cda57cc912ef9a2dbf6c
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-22 16:45:56 -0300

    test: ensure SendResetPasswordEmailController return a bad request if email is invalid

--- src/presentation/controllers/send-reset-password-email/send-reset-password-email.spec.ts
+++ src/presentation/controllers/send-reset-password-email/send-reset-password-email.spec.ts
@@ -1,14 +1,32 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { appError } from '../../helpers/error-helper'
+import { EmailValidator } from '../signup/signup-protocols'
 import { SendResetPasswordEmailController } from './send-reset-password-email'
 
-const makeSut = (): SendResetPasswordEmailController => {
-  return new SendResetPasswordEmailController()
+interface SutTypes {
+  sut: SendResetPasswordEmailController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SendResetPasswordEmailController(emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('SendResetPasswordEmail Controller', () => {
   it('Should return a bad request if email is not provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {}
     }
@@ -17,5 +35,19 @@ describe('SendResetPasswordEmail Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(appError(new MissingParamError('email')))
+  })
+
+  it('Should return a bad request if email is invalid', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(appError(new InvalidParamError('email')))
   })
 })
