089c184728a1a95abb85cc0e3740850b17222a51
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-22 17:11:55 -0300

    feat: ensure SendResetPasswordEmailController call SendResetPasswordEmail with correct email

--- src/presentation/controllers/send-reset-password-email/send-reset-password-email.ts
+++ src/presentation/controllers/send-reset-password-email/send-reset-password-email.ts
@@ -1,3 +1,4 @@
+import { SendResetPasswordEmail } from '../../../domain/usecases/send-reset-password-email'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
@@ -5,9 +6,11 @@ import { EmailValidator } from '../signup/signup-protocols'
 
 export class SendResetPasswordEmailController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly sendResetPasswordEmail: SendResetPasswordEmail
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, sendResetPasswordEmail: SendResetPasswordEmail) {
     this.emailValidator = emailValidator
+    this.sendResetPasswordEmail = sendResetPasswordEmail
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -23,6 +26,8 @@ export class SendResetPasswordEmailController implements Controller {
       if (!isEmailValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      await this.sendResetPasswordEmail.send(email)
     } catch (error) {
       return serverError()
     }
