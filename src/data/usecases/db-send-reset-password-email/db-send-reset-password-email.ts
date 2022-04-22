e7fd12adbb05a52723c58dd828bcd87d7de567a1
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-22 15:51:29 -0300

    feat: ensure DbSendResetPasswordEmail call EmailSender with correct values

--- src/data/usecases/db-send-reset-password-email/db-send-reset-password-email.ts
+++ src/data/usecases/db-send-reset-password-email/db-send-reset-password-email.ts
@@ -3,14 +3,21 @@ import { EmailNotRegisteredError } from '../../errors/email-not-registered-error
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { AccountError } from '../../errors/account-error'
 import { HashGenerator } from '../../protocols/hash-generator'
+import { EmailSender } from '../../protocols/email-sender'
 
 export class DbSendResetPasswordEmail implements SendResetPasswordEmail {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashGenerator: HashGenerator
+  private readonly emailSender: EmailSender
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashGenerator: HashGenerator) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashGenerator: HashGenerator,
+    emailSender: EmailSender
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashGenerator = hashGenerator
+    this.emailSender = emailSender
   }
 
   async send (email: string): Promise<void> {
@@ -24,6 +31,15 @@ export class DbSendResetPasswordEmail implements SendResetPasswordEmail {
       throw new AccountError('Account is inactive', 'AccountIsInactiveError')
     }
 
-    this.hashGenerator.generate()
+    const token = this.hashGenerator.generate()
+
+    await this.emailSender.sendEmail({
+      to: email,
+      from: 'Auth API <reset-password@authapi.com>',
+      subject: 'Authentication API - Defina a sua nova senha',
+      data: {
+        token
+      }
+    })
   }
 }
