ad647e6bbe10c57f569b27120155f1b16f48983d
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-22 16:27:25 -0300

    refactor: move sut creation to a factory function

--- src/presentation/controllers/send-reset-password-email/send-reset-password-email.spec.ts
+++ src/presentation/controllers/send-reset-password-email/send-reset-password-email.spec.ts
@@ -2,9 +2,13 @@ import { MissingParamError } from '../../errors'
 import { appError } from '../../helpers/error-helper'
 import { SendResetPasswordEmailController } from './send-reset-password-email'
 
+const makeSut = (): SendResetPasswordEmailController => {
+  return new SendResetPasswordEmailController()
+}
+
 describe('SendResetPasswordEmail Controller', () => {
   it('Should return a bad request if email is not provided', async () => {
-    const sut = new SendResetPasswordEmailController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com'
