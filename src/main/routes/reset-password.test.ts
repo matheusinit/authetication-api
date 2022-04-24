91f3139d2ffa6b1d417f010324d4b73036f1a8e1
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-24 10:29:19 -0300

    test: ensure SendResetPasswordEmail routes return ok on success

--- src/main/routes/reset-password.test.ts
+++ src/main/routes/reset-password.test.ts
@@ -20,67 +20,86 @@ describe('ResetPassword Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  it('Should return an account on success', async () => {
-    const fakeAccount = {
-      username: 'Matheus Oliveira',
-      email: 'matheus.oliveira@gmail.com',
-      password: await bcrypt.hash('senha123', 12),
-      status: 'active'
-    }
-    await accountCollection.insertOne(fakeAccount)
+  describe('Reset password', () => {
+    it('Should return an account on success', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'active'
+      }
+      await accountCollection.insertOne(fakeAccount)
 
-    await request(app).put('/api/account/reset-password').send({
-      email: 'matheus.oliveira@gmail.com',
-      password: 'Senhaa.123',
-      passwordConfirmation: 'Senhaa.123'
-    }).expect(200)
+      await request(app).put('/api/account/reset-password').send({
+        email: 'matheus.oliveira@gmail.com',
+        password: 'Senhaa.123',
+        passwordConfirmation: 'Senhaa.123'
+      }).expect(200)
+    })
+
+    it('Should return a bad request if email is invalid', async () => {
+      await request(app).put('/api/account/reset-password').send({
+        email: 'matheus.oliveira',
+        password: 'Senhaa.123',
+        passwordConfirmation: 'Senhaa.123'
+      }).expect(400)
+    })
+
+    it('Should return a bad request if password is invalid', async () => {
+      await request(app).put('/api/account/reset-password').send({
+        email: 'matheus.oliveira@gmail.com',
+        password: 'Senha',
+        passwordConfirmation: 'Senha'
+      }).expect(400)
+    })
+
+    it('Should return a bad request if email is not registered', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'active'
+      }
+      await accountCollection.insertOne(fakeAccount)
+
+      await request(app).put('/api/account/reset-password').send({
+        email: 'matheus.oliveira1@gmail.com',
+        password: 'Senhaa.123',
+        passwordConfirmation: 'Senhaa.123'
+      }).expect(400)
+    })
+
+    it('Should return an account on success', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'inactive'
+      }
+      await accountCollection.insertOne(fakeAccount)
+
+      await request(app).put('/api/account/reset-password').send({
+        email: 'matheus.oliveira@gmail.com',
+        password: 'Senhaa.123',
+        passwordConfirmation: 'Senhaa.123'
+      }).expect(400)
+    })
   })
 
-  it('Should return a bad request if email is invalid', async () => {
-    await request(app).put('/api/account/reset-password').send({
-      email: 'matheus.oliveira',
-      password: 'Senhaa.123',
-      passwordConfirmation: 'Senhaa.123'
-    }).expect(400)
-  })
+  describe('Send email with reset password token', () => {
+    it('Should return ok on success', async () => {
+      const fakeAccount = {
+        username: 'Matheus Oliveira',
+        email: 'matheus.oliveira@gmail.com',
+        password: await bcrypt.hash('senha123', 12),
+        status: 'active'
+      }
 
-  it('Should return a bad request if password is invalid', async () => {
-    await request(app).put('/api/account/reset-password').send({
-      email: 'matheus.oliveira@gmail.com',
-      password: 'Senha',
-      passwordConfirmation: 'Senha'
-    }).expect(400)
-  })
+      await accountCollection.insertOne(fakeAccount)
 
-  it('Should return a bad request if email is not registered', async () => {
-    const fakeAccount = {
-      username: 'Matheus Oliveira',
-      email: 'matheus.oliveira@gmail.com',
-      password: await bcrypt.hash('senha123', 12),
-      status: 'active'
-    }
-    await accountCollection.insertOne(fakeAccount)
-
-    await request(app).put('/api/account/reset-password').send({
-      email: 'matheus.oliveira1@gmail.com',
-      password: 'Senhaa.123',
-      passwordConfirmation: 'Senhaa.123'
-    }).expect(400)
-  })
-
-  it('Should return an account on success', async () => {
-    const fakeAccount = {
-      username: 'Matheus Oliveira',
-      email: 'matheus.oliveira@gmail.com',
-      password: await bcrypt.hash('senha123', 12),
-      status: 'inactive'
-    }
-    await accountCollection.insertOne(fakeAccount)
-
-    await request(app).put('/api/account/reset-password').send({
-      email: 'matheus.oliveira@gmail.com',
-      password: 'Senhaa.123',
-      passwordConfirmation: 'Senhaa.123'
-    }).expect(400)
+      await request(app).post('/api/account/reset-password-token').send({
+        email: 'matheus.oliveira@gmail.com'
+      }).expect(200)
+    })
   })
 })
