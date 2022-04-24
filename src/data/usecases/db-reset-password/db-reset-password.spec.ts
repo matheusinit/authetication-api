c4633640c01425fdc844b7423d0f0d3dbf36a568
matheusinit <matheus.oliveira.s@protonmail.com>
2022-04-24 12:43:03 -0300

    test: ensure DbResetPassword throw an error if token lifetime is exactly 24 hours

--- src/data/usecases/db-reset-password/db-reset-password.spec.ts
+++ src/data/usecases/db-reset-password/db-reset-password.spec.ts
@@ -4,6 +4,7 @@ import { DbResetPassword } from './db-reset-password'
 import { AccountError } from '../../errors/account-error'
 import { LoadAccountByTokenRepository } from '../../protocols/load-account-by-token-repository'
 import { NotFoundError } from '../../errors/not-found-error'
+import { InvalidParamError } from '../../../presentation/errors'
 
 interface SutTypes {
   sut: DbResetPassword
@@ -110,6 +111,30 @@ describe('DbResetPassword Usecase', () => {
     const promise = sut.reset('any_hash', 'any_password')
 
     await expect(promise).rejects.toThrow(new AccountError('Account is inactive', 'AccountIsInactiveError'))
+  })
+
+  it('Should throw an error if token lifetime is exactly 24 hours', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(new Promise(resolve => {
+      const tokenCreatedAt = new Date()
+      tokenCreatedAt.setMilliseconds(0)
+
+      tokenCreatedAt.setHours(tokenCreatedAt.getHours() - 24)
+
+      return resolve({
+        id: 'any_id',
+        username: 'any_username',
+        email: 'any_email',
+        password: 'hashed_password',
+        status: 'active',
+        token: 'any_hash',
+        tokenCreatedAt
+      })
+    }))
+
+    const promise = sut.reset('any_hash', 'any_password')
+
+    await expect(promise).rejects.toThrow(new InvalidParamError('token'))
   })
 
   it('Should call Encrypter with correct password', async () => {
